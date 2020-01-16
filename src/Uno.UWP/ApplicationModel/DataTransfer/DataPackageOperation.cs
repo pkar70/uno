bb19c34c05c7235fb9745ab210affc17fa25e17a
Piotr Karocki <pkar@ieee.org>
2020-01-16 11:18:40 +0100

    Create DataPackageOperation.cs

---
+++ src/Uno.UWP/ApplicationModel/DataTransfer/DataPackageOperation.cs
@@ -0,0 +1,14 @@
+// two options are not implemented
+
+namespace Windows.ApplicationModel.DataTransfer
+{
+	public   enum DataPackageOperation 
+	{
+		None,
+		Copy,
+		#if false
+		Move,
+		Link,
+		#endif
+	}
+}
